b59ca7a66f6798f137d008c774b9402afebf7cd3
Brian Bamsch <bbamsch@google.com>
2020-04-21 12:52:28 -0700

    Update test program to open console and print "Hello, World!"
    This program can be dropped in as the /sbin/init program on a root
    filesystem with device special files and will infinitely loop printing
    "Hello, World!" to the console.

--- cross-test.sh
+++ cross-test.sh
@@ -3,8 +3,10 @@
 . ./env.sh
 . ./cross-env.sh
 
-${CC} --target=riscv64-unknown-openbsd6.6 \
+${CC} --target=riscv64-unknown-openbsd6.7 \
 	-march=rv64gc -mcmodel=medany \
-	-nostdinc -idirafter ${destdir}/usr/include \
-	-mno-relax \
-	-c test.c -o test.o
+	-fno-stack-protector \
+	-nostdlib -nostdinc -idirafter ${destdir}/usr/include \
+	-mno-relax -O0 -g \
+	-Wl,-z,notext \
+	test.c -o test
--- test.c
+++ test.c
@@ -7,10 +7,59 @@ __asm("	.section \".note.openbsd.ident\", \"a\"\n"
 "	.long	0\n"
 "	.previous\n");
 
+int	getfd(char *);
+void	print(int, char *, int);
+
 int
 main() {
-	__asm __volatile(
-		"li	t0, 47; /* getgid */	"\
-		"ecall;");
+	int stdoutfd = -1;
+	char stdoutfile[] = {"/dev/console"};
+	while (stdoutfd == -1) {
+		stdoutfd = getfd(stdoutfile);
+	}
+
+	char printbuf[] = {"Hello, World!\n"};
+	int i;
+	for (i = 0; printbuf[i] != 0; i++) {}
+
+	while(1) {
+		print(stdoutfd, printbuf, i);
+	}
+
 	return 0;
+}
+
+int
+getfd(char *fdpath) {
+	int fd;
+	__asm __volatile(
+	"	/* a0 = (char *) fdpath */	"\
+	"	li	a1, 2; /* O_RDWR */	"\
+	"	li	t0, 5; /* open */	"\
+	"	ecall;				"\
+	"	beqz	t0, 1f;			"\
+	"	li	t0, 1; /* exit */	"\
+	"	ecall;				"\
+	"	/* exit should not return? */	"\
+	"1:	mv	%0, a0;			"\
+		: "=r" (fd)
+		: "r" (fdpath));
+	return fd;
+}
+
+void
+print(int fd, char *printbuf, int printbuflen) {
+	__asm __volatile(
+	"	/* a0 = (int) fd */		"\
+	"	/* a1 = (char *) pritnbuf */	"\
+	"	/* a2 = (int) printbuflen */	"\
+	"	li	t0, 4; /* write */	"\
+	"	ecall;				"\
+	"	beqz	t0, 1f;			"\
+	"	li	t0, 1; /* exit */	"\
+	"	ecall;				"\
+	"1:					"\
+		:: "r" (fd),
+		   "r" (printbuf),
+		   "r" (printbuflen));
 }
